456d97d75710dae8ff19b557c5f52e1e3a775e59
Simb-ahh <98308187+Simb-ahh@users.noreply.github.com>
2022-10-16 16:49:51 +0700

    Source: Remote Target: Beta OS: MAC app: PyCharm

---
+++ Coursera/Week 01 - 6 Hello World Quiz.py
@@ -0,0 +1,28 @@
+"""What are functions in Python?
+    ( ) Functions let us use Python as a calculator.
+    ( ) Functions are pieces of code that perform a unit of work.
+    ( ) Functions are only used to print messages to the screen.
+    ( ) Functions are how we tell if our program is functioning or not.
+"""
+
+"""What are keywords in Python?
+    ( ) Keywords are reserved words that are used to construct instructions.
+    ( ) Keywords are used to calculate mathematical operations.
+    ( ) Keywords are used to print messages like "Hello World!" to the screen.
+    ( ) Keywords are the words that we need to memorize to program in Python.
+"""
+
+"""What does the print function do in Python?
+    ( ) The print function generates PDFs and sends it to the nearest printer.
+    ( ) The print function stores values provided by the user.
+    ( ) The print function outputs messages to the screen
+    ( ) The print function calculates mathematical operations."""
+
+'Output a message that says "Programming in Python is fun!" to the screen.'
+x = ""
+print(x)
+
+"""Replace the ___ placeholder and calculate the Golden ratio:  \frac{1+\sqrt{5}}{2} 21+ 5​​.
+Tip: to calculate the square root of a number xx, you can use x**(1/2)."""
+ratio = (1+(2__5)) / __
+print(ratio)
