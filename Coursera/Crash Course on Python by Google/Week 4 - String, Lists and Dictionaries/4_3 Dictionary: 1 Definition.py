b066bfbc8b7c1f78ec676171cf2bf751ec77ed62
Simb-ahh <98308187+Simb-ahh@users.noreply.github.com>
2022-10-21 01:46:53 +0700

    Target: Local[beta] > Py-Learn [beta] OS: MAC app: PyCharm

---
+++ Coursera/Crash Course on Python by Google/Week 4 - String, Lists and Dictionaries/4_3 Dictionary: 1 Definition.py
@@ -0,0 +1,12 @@
+"""
+- Dictionary is used to organize elements into collections
+- Unlike lists, you don't access elements position inside dictionaries using their position
+- The data inside dictionaries take the form of pairs of keys and values
+- To get dictionary value we use its corresponding key
+- While the list index is must be a number, we can assign any data type into as a keys
+"""
+
+"Example 1:"
+x = {}
+type(x)  # output: <class 'dict'>
+file_counts = {'jpg': 10, 'txt': 14, }
