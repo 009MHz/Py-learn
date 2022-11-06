b857ca7e7109965e3b4406166d1024c49eac4e57
Simb-ahh <98308187+Simb-ahh@users.noreply.github.com>
2022-11-06 15:34:27 +0700

    Target: Local -> Py-Learn [beta] OS: MAC app: PyCharm notes:

--- Coursera/Google IT Automation with Python Specialization/2 - Python to Interact With the OS/Week 1 - Getting Ready for Python/1-3 Programming Automation - 3 Automation example.py
+++ Coursera/Google IT Automation with Python Specialization/2 - Python to Interact With the OS/Week 1 - Getting Ready for Python/1-3 Programming Automation - 3 Automation example.py
@@ -2,13 +2,28 @@
 import psutil
 import shutil
 
-# Todo 1: Check Disk usage
-du = shutil.disk_usage('/')  # assigning root folder to check the usage
-print(du)  # usage(total=245107195904, used=105283563520, free=139823632384)
+# Todo 1: define a check_disk_usage function
+def check_disk_usage(disk):
+    du = shutil.disk_usage(disk)  # variable to check the path
+    free = du.free / du.total  # checking the free size ratio vs total size
+    return free > 20  # return as True when the free size > 20 % (healthy size)
 
-# check the disk usage percentage
-percent = du.free / du.total * 100
-print(f"Disk usage: {round(percent, 2)} %")
 
-# Todo 1: Check CPU usage
-psutil.cpu_percent(0.1)  # verifying CPU usage in 0.1 s interval
+# Todo 2: define check_cpu_usage function to check the interval in whole seconds
+def check_cpu_usage():
+    usage = psutil.cpu_percent(1)  # check the CPU usage in 1 second interval
+    return usage < 75  # return as True when the usage is less from 75 (healthy load)
+
+
+# Todo 3: Check if those two values returning false (not in good shape)
+if not check_disk_usage('/') or not check_cpu_usage():
+    print("WARNING !")
+else:
+    print("Verifying PASSED!")
+
+
+# Todo 4:
+"""
+- Extract the code as python file or txt file
+- Run with local console
+"""
