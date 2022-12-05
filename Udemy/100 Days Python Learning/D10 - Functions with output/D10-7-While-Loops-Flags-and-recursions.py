021a9eecc18300361f7b14fe511ecde3e68d7ae0
Simb-ahh <harits.bonnaz@ruangguru.com>
2022-12-06 00:34:47 +0700

    Refactoring calculator part 2

--- Udemy/100 Days Python Learning/D10 - Functions with output/D10-7-While-Loops-Flags-and-recursions.py
+++ Udemy/100 Days Python Learning/D10 - Functions with output/D10-7-While-Loops-Flags-and-recursions.py
@@ -1,6 +1,5 @@
 # MISSION: looping the calculation
-from replit import clear
-# calculator function
+# math operations
 def add(n1, n2):
   return n1 + n2
 
@@ -12,7 +11,7 @@ def multiply(n1, n2):
 
 def divide(n1, n2):
   return n1 / n2
-#math operations
+# converting math ops into symbol
 operations = {
   "+": add,
   "-": subtract,
@@ -20,21 +19,21 @@ operations = {
   "/": divide
 }
 
-def calculator(): #9 recursion, berfungsi untuk mengulangi semuanya dari awal (dari input num1) setelah user mengakhiri sessi sebelumnya seperti di nomor 8 {looping dalam loop}
-    #user interaction
-    num1 = int(input("What's the first number?: ")) #1 mengeluarkan input ini dari looping karena user hanya perlu untuk memberikan input selanjutnya instead input dari awal
+def calculator(): # define calculator mechanism
+    num1 = int(input("What's the first number?: "))  # user interaction yg berada di luar looping process
     
-    calculation = True #2 variable kunci utk looping
+    calculation = True # looping flags condition
     while calculation:
-        operation_symbol = input("Pick an operation: ") #3 start looping, yaitu proses input operation simbol
-        num2 = int(input("What's the next number?: ")) #4 next looping, input nomor selanjutnya, dan ketika ini di looping akan tetap berfungsi sebagai num 2 (bukan num 3,4 dst) karena num 1 sudah dikeluarkan dari looping (liat no7)
-        calculation_function = operations[operation_symbol]
-        answer = calculation_function(num1,num2)
-        print(f"{num1} {operation_symbol} {num2} = {answer}")
-        proceed = input(f'"Type "y" to continue calculate with {answer}, type "n" to exit\n') #5 validasi user untuk melanjutkan kalkulasi  / tidak
-        if proceed == "y": #6 setting kondisi jika user lanjut
-            num1 = answer #7 jika lanjut, jawaban user sebelumnya akan menggantikan num1 yang dikeluarkan dari looping supaya num1 meneruskan jawaban user terakhir instead angka sewaktu starting
+        operation_symbol = input("Pick an operation: ")
+        num2 = int(input("What's the next number?: "))  # berfungsi sebagai second argumen
+        calculation_function = operations[operation_symbol]  # converting symbol input to the function name
+        answer = calculation_function(num1,num2)  # math operations based on both user arguments
+        print(f"{num1} {operation_symbol} {num2} = {answer}")  # displaying the output
+        if input(f'Last ouput: {answer}\nType "y" to continue, type "n" to restart the calculation\n') == "y": # prompt to continue or restart the programs
+            num1 = answer  # converting last output into first argument math function
         else:
-            calculation = False #8 kondisi untuk mengakhiri sessi kalkulator yang ada
-            calculator() #10 jika tidak memanggil fungsi ini di kondisi else, maka sesi akan benar-benar berakhir tanpa bisa menggunakan fungsi kalkulator untuk sessi selanjutnya
-calculator() #11 jika tidak memanggil fungsi ini di luar definisi fungsi yang diberikan (dari num1 - proceed) maka kalkulator tidak akan bisa dijalankan, karena fungsi nya cuman diberikan tapi tidak digunakan
+            calculation = False  # ending current session
+            calculator() # recall the calculator function to restart the program (recursion)
+
+
+calculator()  # using the calculator programs
