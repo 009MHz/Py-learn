12364a5bdbd134bdd3dbf54d32e3c44cc5d1b9f8
009MHz <mharisbsatriyo@gmail.com>
2022-12-07 23:47:53 +0700

    Quiz Project Updated

---
+++ Udemy/100 Days Python Learning/D17 - OOP Benefits & Quiz Project/Quiz Project/Quiz_data.py
@@ -0,0 +1,33 @@
+question_data = [
+
+    {
+        "question": "The song 'Megalovania' by Toby Fox made its third appearence in the 2015 RPG 'Undertale'.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "question": "The game 'Battlefield 1' takes place during World War I.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "question": "'Half-Life 2' runs on the Source Engine.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "question": "In Kingdom Hearts the Paopu fruit is said to intertwine the destinies for two people forever.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "question": "Gordon Freeman, the protagonist of 'Half-Life', is said to have once had a ponytail.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "question": "In Pok&eacute;mon Sun and Moon, a male Salandit can evolve to a Salazzle.",
+        "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "question": "The Konami Code is known as Up, Up, Down, Down, Left, Right, Right, Left, B, A, Start.",
+        "correct_answer": "False", "incorrect_answers": ["True"]},
+    {
+        "question": "The game 'Pocket Morty' has a Morty called 'Pocket Mortys Morty'?",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "question": "In the video game 'Splatoon', the playable characters were originally going to be rabbits instead of squids.",
+        "correct_answer": "True", "incorrect_answers": ["False"]},
+    {
+        "question": "The game 'Jetpack Joyride' was created by 'Redbrick Studios'.",
+        "correct_answer": "False", "incorrect_answers": ["True"]}
+]
---
+++ Udemy/100 Days Python Learning/D17 - OOP Benefits & Quiz Project/Quiz Project/Quiz_games.py
@@ -0,0 +1,23 @@
+from Quiz_data import question_data
+from Quiz_flow import Brain
+
+class Question:
+    """Data List converter to Class object"""
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+        
+
+question_bank = []
+for x in question_data:
+    question_text = x['question']
+    answer_text = x['correct_answer']
+    new_question = Question(question_text, answer_text)
+    question_bank.append(new_question)
+    
+
+quiz = Brain(question_bank)
+quiz.next_q()
+
+print("You've completed the quiz")
+print(f"Your final score is: {quiz.point}/{quiz.question_number}")
