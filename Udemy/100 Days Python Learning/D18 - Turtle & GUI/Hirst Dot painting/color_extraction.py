b0af87735c0f562bc46164191631a47a45f9262c
009MHz <mharisbsatriyo@gmail.com>
2022-12-10 21:30:25 +0700

    adding hirst images and color extraction

---
+++ Udemy/100 Days Python Learning/D18 - Turtle & GUI/Hirst Dot painting/color_extraction.py
@@ -0,0 +1,18 @@
+# # reference: https://pypi.org/project/colorgram.py/
+# import colorgram
+# colors = colorgram.extract("hirst_sample.jpg", 25)  # extracting color data from images as default module output
+#
+# # converting extraction value into readable turtle color format
+# color_extraction = []
+# for x in colors:
+#     r = x.rgb.r
+#     g = x.rgb.g
+#     b = x.rgb.b
+#     extracted = (r,g,b)
+#     color_extraction.append(extracted)
+# print(color_extraction)
+
+"""Comment out previous script after extracted color have been obtained and filtered"""
+color_list = [
+    (26, 108, 164), (193, 38, 81), (237, 161, 50), (234, 215, 86), (227, 237, 229), (223, 137, 176), (143, 108, 57), (103, 197, 219), (21, 57, 132), (205, 166, 30), (213, 74, 91), (238, 89, 49), (142, 208, 227), (119, 191, 139), (5, 185, 177), (106, 108, 198), (137, 29, 72), (4, 162, 86), (98, 51, 36), (24, 155, 210), (229, 168, 185), (173, 185, 221)
+]
