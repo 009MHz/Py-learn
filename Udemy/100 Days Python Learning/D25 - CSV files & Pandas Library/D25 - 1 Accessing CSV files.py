1c141437839cbc46d1914b2e7df6412e9e7025d0
009MHz <mharisbsatriyo@gmail.com>
2022-12-16 20:54:59 +0700

    accessing CSV files using python

--- Udemy/100 Days Python Learning/D25 - CSV files & Pandas Library/D25 - 1 Accessing CSV files.py
+++ Udemy/100 Days Python Learning/D25 - CSV files & Pandas Library/D25 - 1 Accessing CSV files.py
@@ -1,11 +1,12 @@
 """Accessing the RAW csv files"""
-# with open("weather_data.csv") as file:
-#     data = file.readlines()
-#     print(data)
+with open("weather_data.csv") as file:
+    data = file.readlines()
+    print(data)
+
 
 """Accessing CSV files with CSV modules"""
 import csv
-
+# my approaches
 with open("weather_data.csv") as file:
     data = csv.reader(file)
     #print(data)  # <_csv.reader object at 0x000000EC9FC1E320>
@@ -15,3 +16,21 @@ with open("weather_data.csv") as file:
             if temp.isnumeric():
                 temperatures.append(temp)
         print(temperatures)
+
+# course approaches
+with open("weather_data.csv") as file:
+    data = csv.reader(file)
+    temperatures = []
+    for x in data:
+        if x[1] != "temp":  # condition to exclude row title
+            temperatures.append(int(x[1]))  # inserting the values in the second row contents into empty list
+    print(temperatures)
+
+
+"""Accessing CSV files with pandas module"""
+import pandas
+
+data = pandas.read_csv("weather_data.csv")  # assign variable to read the CSV files
+print(data)  # displaying the whole contents from CSV files
+
+print(data["temp"])  # displaying specific contents by calling the title name
