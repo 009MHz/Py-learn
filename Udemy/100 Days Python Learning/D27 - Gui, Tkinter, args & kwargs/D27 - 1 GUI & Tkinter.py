c5df2dac44e5e2d90621e9984e278bee6984e67b
009MHz <mharisbsatriyo@gmail.com>
2022-12-28 10:53:06 +0700

    d27 start

---
+++ Udemy/100 Days Python Learning/D27 - Gui, Tkinter, args & kwargs/D27 - 1 GUI & Tkinter.py
@@ -0,0 +1,10 @@
+import tkinter
+
+# creating the GUI screen
+screen = tkinter.Tk()
+
+# providing windows title
+screen.title("Tkinter GUI title")
+
+# Make the screen always visible
+screen.mainloop()
