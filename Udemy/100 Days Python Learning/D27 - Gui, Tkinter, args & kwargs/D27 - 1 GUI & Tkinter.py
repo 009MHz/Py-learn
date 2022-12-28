37492d718a44032ee54bad3343e6e83741733b67
009MHz <mharisbsatriyo@gmail.com>
2022-12-28 11:59:46 +0700

    d27 tkinter advance

--- Udemy/100 Days Python Learning/D27 - Gui, Tkinter, args & kwargs/D27 - 1 GUI & Tkinter.py
+++ Udemy/100 Days Python Learning/D27 - Gui, Tkinter, args & kwargs/D27 - 1 GUI & Tkinter.py
@@ -1,10 +1,18 @@
 import tkinter
 
-# creating the GUI screen
+"""Creating the GUI screen"""
 screen = tkinter.Tk()
 
-# providing windows title
+"""Providing window Content"""
+# GUI window title
 screen.title("Tkinter GUI title")
 
-# Make the screen always visible
+# GUI window size
+screen.minsize(width = 480, height = 500)
+
+# adding window label
+win_lab = tkinter.Label(text="Tkinter GUI Label", font=("Lato", 24,"bold"))  # define the content
+win_lab.pack()  # showing the label code (at the top center inside the windows = default)
+
+"""Make the screen always visible"""
 screen.mainloop()
